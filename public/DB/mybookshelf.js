18510a9dedab145be362e33f695c2c68a683c444
이영민 <trustme@iyeongmin-ui-MacBook-Pro.local>
2017-04-07 05:38:01 +0900

    add detail view and css modify

--- public/DB/mybookshelf.js
+++ public/DB/mybookshelf.js
@@ -4,54 +4,77 @@ var my_bookshelf = [
         "id": "1",
         "title": "칼의 노래",
         "author": "김훈",
-        "src": "http://books.google.com/books/content?id=G_9KAQAAIAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api"
+        "src": "http://books.google.com/books/content?id=G_9KAQAAIAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api",
+        "review": "lorem ipsum borias viato, senore cliaqilo fooiel mialwes aadioes qotouse palram ssdqqw"
     },
     {   
         "id": "2",
         "title": "네가 누구든 얼마나 외롭든",
         "author": "김연수",
-        "src": "http://books.google.com/books/content?id=dvlKAQAAIAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api"
+        "src": "http://books.google.com/books/content?id=dvlKAQAAIAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api",
+        "review": "lorem ipsum borias viato, senore cliaqilo fooiel mialwes aadioes qotouse palram ssdqqw"
     },
     {   
         "id": "3",
         "title": "기형도 전집",
         "author": "기형도",
-        "src": "http://books.google.com/books/content?id=gf9KAQAAIAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api"
+        "src": "http://books.google.com/books/content?id=gf9KAQAAIAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api",
+        "review": "lorem ipsum borias viato, senore cliaqilo fooiel mialwes aadioes qotouse palram ssdqqw"
     },
     {   
         "id": "4",
         "title": "실내인간",
         "author": "이석원",
-        "src": "http://books.google.com/books/content?id=rEndAgAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
+        "src": "http://books.google.com/books/content?id=rEndAgAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+        "review": "lorem ipsum borias viato, senore cliaqilo fooiel mialwes aadioes qotouse palram ssdqqw"
     },
     {   
         "id": "5",
         "title": "언제 들어도 좋은말",
         "author": "이석원",
-        "src": "http://books.google.com/books/content?id=UD6dCgAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
+        "src": "http://books.google.com/books/content?id=UD6dCgAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+        "review": "lorem ipsum borias viato, senore cliaqilo fooiel mialwes aadioes qotouse palram ssdqqw"
     },
     {   
         "id": "6",
         "title": "한국이 싫어서",
         "author": "장강명",
-        "src": "http://books.google.com/books/content?id=fEHcCQAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
+        "src": "http://books.google.com/books/content?id=fEHcCQAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+        "review": "lorem ipsum borias viato, senore cliaqilo fooiel mialwes aadioes qotouse palram ssdqqw"
     },
     {   
         "id": "7",
         "title": "인간의 조건",
         "author": "앙드레 말로",
-        "src": "http://books.google.com/books/content?id=rtLODAAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
+        "src": "http://books.google.com/books/content?id=rtLODAAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+        "review": "lorem ipsum borias viato, senore cliaqilo fooiel mialwes aadioes qotouse palram ssdqqw"
     },
     {   
         "id": "8",
         "title": "현의 노래",
         "author": "김훈",
-        "src": "http://books.google.com/books/content?id=jP5KAQAAIAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api"
+        "src": "http://books.google.com/books/content?id=jP5KAQAAIAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api",
+        "review": "lorem ipsum borias viato, senore cliaqilo fooiel mialwes aadioes qotouse palram ssdqqw"
     },
     {   
-        "id": "9",
-        "title": "소란",
-        "author": "박연주",
-        "src": "../asset/소란.jpg"
-    }
+        "id": "5",
+        "title": "언제 들어도 좋은말",
+        "author": "이석원",
+        "src": "http://books.google.com/books/content?id=UD6dCgAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+        "review": "lorem ipsum borias viato, senore cliaqilo fooiel mialwes aadioes qotouse palram ssdqqw"
+    },
+    {   
+        "id": "6",
+        "title": "한국이 싫어서",
+        "author": "장강명",
+        "src": "http://books.google.com/books/content?id=fEHcCQAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+        "review": "lorem ipsum borias viato, senore cliaqilo fooiel mialwes aadioes qotouse palram ssdqqw"
+    },
+    {   
+        "id": "7",
+        "title": "인간의 조건",
+        "author": "앙드레 말로",
+        "src": "http://books.google.com/books/content?id=rtLODAAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+        "review": "lorem ipsum borias viato, senore cliaqilo fooiel mialwes aadioes qotouse palram ssdqqw"
+    },
 ]
--- public/js/my-page.js
+++ public/js/my-page.js
@@ -1,4 +1,23 @@
-function clickStar() {
+
+// Select Background
+// DOM API, CSS 선택자 중에 클래스 선택자로 대상을 선택
+var color_list       = document.querySelector('.menu-colorlist')
+var bg_color         = document.body;
+var color_list_items = color_list.querySelectorAll('.color-option');
+// console.log(color_list_items);
+// 배경 색상 변경
+for (var i = 0; i < color_list_items.length; i++) {
+    color_list_items[i].onclick = function() {
+    var my_color = window.getComputedStyle(this,null).background;
+    // console.log(my_color);
+    bg_color.style.background = my_color;
+    return false;
+    };
+};
+
+
+// Star Rating
+function starRating() {
     var star = document.querySelectorAll('span');
     console.log(star);
 
@@ -6,8 +25,8 @@ function clickStar() {
         // var beforestar = document.querySelector('span:before')
         // console.log(beforestar);
         star[i].onclick = function() {
-            console.log('clicked');
-            console.log(this);
+            // console.log('clicked');
+            // console.log(this);
             var hoverstar = document.querySelector('span:hover');
             console.log(hoverstar);
             var beforestar = document.querySelector('::before');
@@ -17,20 +36,28 @@ function clickStar() {
         }
     }
 }
-clickStar();
+starRating();
 
-var vm = new Vue ({
-    el: '#app',
-    data: {
-        is_visible: true,
-        books: my-bookshelf
-    },
-    methods: {
-        clicklistview: function(){
-            this.is_visible = true;
-        },
-        clickdetailview: function(){
-            this.is_visible = false
-        }
+
+if (star[i]) {
+    switch (star[i]) {
+        case '1':
+            hoverstar.style.cssText = "color : gold";
+            break;
+        case '2':
+            hoverstar.style.cssText = "color : gold";
+            break;
+        case '3':
+            hoverstar.style.cssText = "color : gold";
+            break;
+        case '4':
+            hoverstar.style.cssText = "color : gold";
+            break;
+        case '5':
+            hoverstar.style.cssText = "color : gold";
+            break;
+        default:
+            hoverstar.style.cssText = "color : none";
+            break;
     }
-})
+}
